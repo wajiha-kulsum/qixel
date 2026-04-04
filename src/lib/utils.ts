1f01ba5dff6eb6a30913df0847f4967605884115
wajiha-kulsum <wajihakulsum786@gmail.com>
2026-04-04 15:47:32 +0530

    refactor(lib): add explicit jsdoc comments to utility functions

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,13 @@
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
 
+/**
+ * Merges class names intelligently using clsx and tailwind-merge.
+ * Resolves tailwind class conflicts and conditionally applies classes.
+ * 
+ * @param inputs - Array of class values to be merged.
+ * @returns A string of merged tailwind classes.
+ */
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
